429037e85a744c367f0eb91228d0504e1d3b3a28
patelutkarsh97 <47344300+patelutkarsh97@users.noreply.github.com>
2019-02-19 10:21:18 +0530

    UTKARSH PATEL
    Login Page - Part of iOS Assignment 1

---
+++ LoginPage/LoginPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginPage
+//
+//  Created by Utkarsh Patel on 18/02/19.
+//  Copyright © 2019 Utkarsh Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
+
