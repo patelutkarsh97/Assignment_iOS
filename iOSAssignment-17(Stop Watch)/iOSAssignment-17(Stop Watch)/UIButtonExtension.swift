1ab7c73fb9e516bc3f4f78861d9a2af2480f2b40
patelutkarsh97 <47344300+patelutkarsh97@users.noreply.github.com>
2019-03-20 23:06:04 +0530

    iOS Assignment 17 Multi-Threading-1
    Session : iOS : Multi-Threading (GCD & NSOperationQueue) - 1
    iOSAssignment 17

---
+++ iOSAssignment-17(Stop Watch)/iOSAssignment-17(Stop Watch)/UIButtonExtension.swift
@@ -0,0 +1,50 @@
+//
+//  UIButtonExtension.swift
+//  iOSAssignment-17(Stop Watch)
+//
+//  Created by Utkarsh Patel on 20/03/19.
+//  Copyright © 2019 Utkarsh Patel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+extension UIButton {
+    func pulsate() {
+        let pulse = CASpringAnimation(keyPath: "transform.scale")
+        pulse.duration = 0.6
+        pulse.fromValue = 0.95
+        pulse.toValue = 1.0
+        pulse.autoreverses = true
+        pulse.repeatCount = 2
+        pulse.initialVelocity = 0.5
+        pulse.damping = 1.0
+        layer.add(pulse, forKey: nil)
+    }
+    
+    func flash() {
+        let flash = CASpringAnimation(keyPath: "opacity")
+        flash.duration = 0.5
+        flash.fromValue = 1
+        flash.toValue = 0.1
+        flash.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        flash.autoreverses = true
+        flash.repeatCount = 2
+        layer.add(flash, forKey: nil)
+    }
+    
+    func shake() {
+        let shake = CASpringAnimation(keyPath: "position")
+        shake.duration = 0.1
+        shake.repeatCount = 2
+        shake.autoreverses = true
+        let fromPoint = CGPoint(x: center.x - 5, y: center.y)
+        let fromValue = NSValue(cgPoint: fromPoint)
+        let toPoint = CGPoint(x: center.x + 5, y: center.y)
+        let toValue = NSValue(cgPoint: toPoint)
+        shake.fromValue = fromValue
+        shake.toValue = toValue
+        layer.add(shake, forKey: nil)
+    }
+}
+
+
